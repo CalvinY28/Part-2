7d68b0604b4610270e1cbe23e8824fde2064a845
yangcal <yangcal@OA-S141-09>
2024-02-07 12:49:12 -0500

    Task 4a work in progress

---
+++ Assets/Week 5/Scripts/Weapon.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float daggerSpeed = 5f;
+    Rigidbody2D rb;
+    GameObject dagger;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(0 , daggerSpeed * Time.deltaTime, 0);
+
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
