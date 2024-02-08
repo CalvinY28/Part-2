d599f71e44af61ea83f0c59b50424c066fc6590e
CalvinY28 <calvinoct28@gmail,com>
2024-02-07 23:30:03 -0500

    Task4a semi complete: Created weapon prefab, used SendMessge, Destroy weapon. However health does not go down.

--- Assets/Week 5/Scripts/Weapon.cs
+++ Assets/Week 5/Scripts/Weapon.cs
@@ -4,33 +4,40 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public float daggerSpeed = 5f;
-    Rigidbody2D rb;
-    GameObject dagger;
+    public float daggerSpeed = 10f;
+    public float damage = 1f;
+    //Rigidbody2D rb;
+    //GameObject dagger;
+    //Vector2 movement;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        //Destroy(gameObject, 1f); using destory when off screen instead
+        //rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
-        transform.Translate(0 , daggerSpeed * Time.deltaTime, 0);
+        transform.Translate(0, daggerSpeed * Time.deltaTime, 0);
 
     }
 
     private void OnBecameInvisible()
     {
-        Destroy(gameObject);
+        Destroy(gameObject); // when off screen destroy instead of using a timer
     }
 
-    public void OnCollisionEnter2D(Collision2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
+        collision.SendMessage("TakeDMG", damage, SendMessageOptions.DontRequireReceiver); // incorrect * getting error no reciever ** SendMessageOptions.DontRequireReceiver is used
         Destroy(gameObject);
     }
-}//test
+    //public void TakeDMG(float damage) { }
+
+
+}
