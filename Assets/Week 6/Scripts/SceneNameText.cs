f4f6b1178c093626be1571caa2162e5e9cbedf62
yangcal <yangcal@OA-S141-09>
2024-02-14 10:30:56 -0500

    Task 1b complete script changing text to scene name and a button that loads next scene

---
+++ Assets/Week 6/Scripts/SceneNameText.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SceneNameText : MonoBehaviour
+{
+    TextMeshProUGUI sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneName = GetComponent<TextMeshProUGUI>();
+        sceneName.text = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
