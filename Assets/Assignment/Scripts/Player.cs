009addf6d31a0f2fd40079558af3f468e718f527
CalvinY28 <calvinoct28@gmail.com>
2024-02-20 10:05:10 -0500

    Functionality 2: Added health bar UI above player where you can take damage and die. Also fixed animations.

--- Assets/Assignment/Scripts/Player.cs
+++ Assets/Assignment/Scripts/Player.cs
@@ -4,17 +4,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.Tilemaps;
+using UnityEditor.Experimental.GraphView;
 
 public class Player : MonoBehaviour
 {
+    private float currentHealth;
+    public float maxHealth = 5f;
     public float moveSpeed = 5f;
-    private bool isMoving = false;
-    private Vector3 targetPosition;
+    //private bool isMoving = false;
+    private Vector3 targetPosition; //I should probably utilize Vector2
     public Animator animator;
+    public Slider healthSlider;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        currentHealth = maxHealth;
+        UpdateHealthSlider();
     }
 
     void Update()
@@ -26,14 +32,23 @@ public class Player : MonoBehaviour
         if (Input.GetMouseButton(1)) {
             Vector3 targetPosition = GetMouseWorldPosition();
             MoveTo(targetPosition);
+            animator.SetBool("WalkRight", true);
         }
 
-        if (isMoving) { // I wanted to make it so the player moves until the targetPosition is reached but something is wrong. It is fine, movement works without rigid
-            float distanceToClick = Vector3.Distance(transform.position, targetPosition);
-            if (distanceToClick > 0.1f) {
-                isMoving = false;
-            }
+        if (Input.GetMouseButtonUp(1)) {
+            animator.SetBool("WalkRight", false);
         }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            animator.SetTrigger("Roll");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.SendMessage("TakeDamage", 1);
+        }
+
     }
 
     void MoveTo(Vector3 targetPosition) { 
@@ -43,7 +58,8 @@ public class Player : MonoBehaviour
 
         Vector3 direction = (targetPosition - transform.position).normalized; //Normalize to make sure Vector is same length i think (Does not work without normalizing)
         transform.Translate(direction * moveSpeed * Time.deltaTime);
-        isMoving = true;
+        //isMoving = true;
+        float distanceToClick = Vector3.Distance(transform.position, targetPosition); //This feels more complicated then it needs to be
     }
 
     Vector3 GetMouseWorldPosition() { 
@@ -56,4 +72,30 @@ public class Player : MonoBehaviour
         return Camera.main.ScreenToWorldPoint(mousePosition);
     }
 
+    void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+        UpdateHealthSlider();
+
+        if (currentHealth <= 0f) {
+            Die();
+            animator.SetTrigger("Death");
+        }
+        else {
+            animator.SetTrigger("TakeDMG");
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player died!");
+    }
+
+    void UpdateHealthSlider()
+    {
+        if (healthSlider != null) {
+            healthSlider.value = currentHealth;
+        }
+    }
 }
