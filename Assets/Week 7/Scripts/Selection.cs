c2996438d16487701c7ea6a086eda0b8af4c9b95
yangcal <yangcal@OA-S141-09>
2024-02-21 11:00:01 -0500

    Task 2 Complete: selecting one player. and added physcis to flick player to mouse

--- Assets/Week 7/Scripts/Selection.cs
+++ Assets/Week 7/Scripts/Selection.cs
@@ -7,19 +7,22 @@ using UnityEngine;
 public class Selection : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
+    public Rigidbody2D rb;
     public Color selectedColour;
     public Color unSelectedColour;
+    public float speed = 10f;
 
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
         Selected(false);
     }
 
     private void OnMouseDown()
     {
-        Selected(true);
+        Controller.SetSelectedPlayer(this);
     }
 
     public void Selected(bool isSelected) { 
@@ -31,6 +34,10 @@ public class Selection : MonoBehaviour
         {
             spriteRenderer.color = unSelectedColour;
         }
+    }
+
+    public void Move(Vector2 direction) { 
+        rb.AddForce(direction * speed, ForceMode2D.Impulse);
     }
 
 }
